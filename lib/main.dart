0671f84b44e03abe70109d4d9556233e8f4accdd
OmegaDracul <salimabdu008@gmail.com>
2018-09-15 13:59:23 +0100

    nothing new

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      
       home: Scaffold(
         appBar: AppBar(
             title: Title(
